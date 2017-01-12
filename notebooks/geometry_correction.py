ff42213b4f360c6073d5e9eca47534298c832fea
JeanBilheux <bilheuxjm@ornl.gov>
2017-01-12 16:53:43 -0500

    Finished first draft of implementation of homo and inhomogeneous cylindrical geometry corrections

--- notebooks/geometry_correction.py
+++ notebooks/geometry_correction.py
@@ -8,3 +8,15 @@ def homogeneous_correction_factor(x=0, radius=-1):
     if x == 0:
         return 1
     return (2*radius)/rp
+
+def inhomogeneous_correction(x=-1, inner_circle_r=1, outer_circle_r=1):
+    r = np.abs(x)
+    if r >= outer_circle_r:
+        return 0
+    elif (r >= inner_circle_r) and (r <= outer_circle_r):
+        return 2*outer_circle_r*np.sin(np.arccos(x/outer_circle_r))
+    else:
+        rp1 = 2*inner_circle_r*np.sin(np.arccos(x/inner_circle_r))
+        rp2 = 2*outer_circle_r*np.sin(np.arccos(x/outer_circle_r))
+        rp = rp2 - rp1
+        return rp
